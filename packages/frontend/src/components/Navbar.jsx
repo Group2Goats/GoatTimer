25eda658b0b1b0d70af6b8bbbe2569cc0a5373b6
Alfredo <193377562+A-Galicia@users.noreply.github.com>
2026-04-29 11:37:46 -0700

    feat: links from navbar

--- packages/frontend/src/components/Navbar.jsx
+++ packages/frontend/src/components/Navbar.jsx
@@ -1,50 +1,64 @@
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import { Link } from "react-router";
+import styles from "./Navbar.module.css";
 
 function Navbar() {
   return (
-    <NavigationMenu.Root className="NavRoot">
-      <NavigationMenu.List className="NavList">
+    <NavigationMenu.Root className={styles.NavRoot}>
+      <NavigationMenu.List className={styles.NavList}>
         <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="NavTrigger">
-            Focus
+          <NavigationMenu.Trigger className={styles.NavTrigger}>
+            <NavigationMenu.Link className={styles.NavLink} href="signup">
+              Focus
+            </NavigationMenu.Link>
           </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="NavContent">
-            <div className="DropdownCard">Distraction free study sessions.</div>
+          <NavigationMenu.Content className={styles.NavContent}>
+            <div className={styles.DropdownCard}>
+              Distraction free study sessions.
+            </div>
           </NavigationMenu.Content>
         </NavigationMenu.Item>
 
         <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="NavTrigger">
-            Compete
+          <NavigationMenu.Trigger className={styles.NavTrigger}>
+            <NavigationMenu.Link className={styles.NavLink} href="signup">
+              Compete
+            </NavigationMenu.Link>
           </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="NavContent">
-            <div className="DropdownCard">
+          <NavigationMenu.Content className={styles.NavContent}>
+            <div className={styles.DropdownCard}>
               Track goals and finish what you planned.
             </div>
           </NavigationMenu.Content>
         </NavigationMenu.Item>
 
         <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="NavTrigger">
-            Win
+          <NavigationMenu.Trigger className={styles.NavTrigger}>
+            <NavigationMenu.Link className={styles.NavLink} href="signup">
+              Win
+            </NavigationMenu.Link>
           </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="NavContent">
-            <div className="DropdownCard">
+          <NavigationMenu.Content className={styles.NavContent}>
+            <div className={styles.DropdownCard}>
               Turn consistency into real progress.
             </div>
           </NavigationMenu.Content>
         </NavigationMenu.Item>
 
         <NavigationMenu.Item>
-          <NavigationMenu.Link className="NavLink" href="about">
-            About
-          </NavigationMenu.Link>
+          <NavigationMenu.Trigger className={styles.NavTrigger}>
+            <NavigationMenu.Link className={styles.NavLink} href="about">
+              About
+            </NavigationMenu.Link>
+          </NavigationMenu.Trigger>
+          <NavigationMenu.Content className={styles.NavContent}>
+            <div className={styles.DropdownCard}>Learn More!</div>
+          </NavigationMenu.Content>
         </NavigationMenu.Item>
       </NavigationMenu.List>
 
-      <div className="ViewportPosition">
-        <NavigationMenu.Viewport className="NavViewport" />
+      <div className={styles.ViewportPosition}>
+        <NavigationMenu.Viewport className={styles.NavViewport} />
       </div>
     </NavigationMenu.Root>
   );
